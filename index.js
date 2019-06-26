f7c6e1f8b70dda380168f9a6fdaa4dc94896c4d3
Jackmc <jackmc1122@gmail.com>
2019-06-26 14:42:34 -0700

    Send different html files based on url.pathname and test that buddy is inside the html being sent.

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-const { createServer } = require('http');
+const app = require('./lib/app');
 
-const server = createServer((req, res) => {
-  console.log('client made a req');
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-      h1 {
-        color: red;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Hello There Site Visitor</h1>
-    <script>
-      console.log('I am a script');
-    </script>
-  </body>
-  </html>`);
-});
-
-server.listen(7890, () => {
+app.listen(7890, () => {
   console.log('Listening');
 });
